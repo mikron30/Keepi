f2d3eadbdbfd568da59f40544ed838980e2e2f13
mikron30 <53993027+mikron30@users.noreply.github.com>
2026-09-23 18:06:57 +0300

    Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/keepi_app.dart';
 import 'core/firebase/firebase_bootstrap.dart';
 import 'core/firebase/firebase_providers.dart';
+import 'core/theme/theme_preference.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await ThemePreference.load();
   final firebaseReady = await FirebaseBootstrap.tryInitialize();
 
   runApp(
