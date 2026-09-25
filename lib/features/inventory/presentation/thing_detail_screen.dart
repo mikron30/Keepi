5c24efd7813dd9a97b784a5f3775f866c97d8971
mikron30 <53993027+mikron30@users.noreply.github.com>
2026-09-25 19:50:56 +0300

    Display custom category names on product pages

--- lib/features/inventory/presentation/thing_detail_screen.dart
+++ lib/features/inventory/presentation/thing_detail_screen.dart
@@ -318,7 +318,15 @@ class _ThingDetailScreenState extends State<ThingDetailScreen> {
       case 'personal_care':
         return 'Personal care';
       default:
-        return 'Other';
+        return value
+            .split('_')
+            .where((part) => part.isNotEmpty)
+            .map(
+              (part) => part.isEmpty
+                  ? part
+                  : '${part[0].toUpperCase()}${part.substring(1)}',
+            )
+            .join(' ');
     }
   }
 
