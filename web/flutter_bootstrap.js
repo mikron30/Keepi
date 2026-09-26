5c2c256709977f5b88d5caac0d2b74cfdd8fc790
mikron30 <53993027+mikron30@users.noreply.github.com>
2026-09-26 17:34:12 +0300

    Restore normal Flutter bootstrap for PWA installability

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,22 +1,4 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-async function startKeepi() {
-  if ('serviceWorker' in navigator) {
-    try {
-      const registrations = await navigator.serviceWorker.getRegistrations();
-      await Promise.all(registrations.map((registration) => registration.unregister()));
-
-      if ('caches' in window) {
-        const cacheNames = await caches.keys();
-        await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
-      }
-    } catch (error) {
-      console.warn('Keepi cache cleanup warning:', error);
-    }
-  }
-
-  await _flutter.loader.load();
-}
-
-startKeepi();
+_flutter.loader.load();
